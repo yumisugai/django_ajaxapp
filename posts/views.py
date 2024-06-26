ed5bcb2bf4d374a3a2f7394257db176830e1ccbd
yumisugai <yumi-sugai@tokyu-hl.jp>
2024-06-26 10:16:32 +0900

    AjaxAppの準備

--- posts/views.py
+++ posts/views.py
@@ -5,8 +5,15 @@ from .forms import PostForm
 
 class IndexView(View):
     def get(self,request,*args,**kwargs):
-        posts =  Post.objects.all().order_by('-created_at')
-        return render(request, 'posts/index.html',{'posts':posts})
+        posts =  Post.objects.all().all().order_by('-created_at')
+        form = PostForm()
+        return render(request, 'posts/index.html', {'posts': posts, 'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
     
 class CreateView(View):
     def get(self, request, *args, **kwargs):
